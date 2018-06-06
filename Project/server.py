98dd1a996506d4d66abf0457566a82d9bcb6e475
Ahmadreza.smdi <ahmadreza.smdi@gmail.com>
2018-06-06 20:24:32 +0430

    Almost over

--- Project/server.py
+++ Project/server.py
@@ -4,7 +4,10 @@ import os
 import random
 import string
 import sqlite3
+import time
 
+
+date = str(time.asctime( time.localtime(time.time()) ))
 class BaseHandler(tornado.web.RequestHandler):
     def get_current_user(self):
         return self.get_secure_cookie("username")
@@ -27,10 +30,11 @@ class setting(BaseHandler):
         query = 'SELECT * FROM "member" WHERE "m_username"=?;'
         cursor = self.application.db.execute(query,[current_user])
         res = cursor.fetchone()
-        self.render("settings.html",val=res)
+        self.render("settings.html",val=res,change='false')
    
     @tornado.web.authenticated
     def post(self):
+        current_user = str(self.current_user, encoding='utf-8')
         name = self.get_argument('name')
         username = self.get_argument('username')
         password = self.get_argument('password')
@@ -40,12 +44,43 @@ class setting(BaseHandler):
         bio = self.get_argument('bio')
         birthdate = self.get_argument('birthdate')
         agreement = self.get_argument('agreement')
+
+        query = 'SELECT * FROM "member" WHERE "m_username"=?;'
+        cursor = self.application.db.execute(query,[current_user])
+        res = cursor.fetchone()
+        call_signin = False
+        if res[1]!=username or res[2]!=password :
+            call_signin = True
+
         query="UPDATE 'member' SET 'm_name'=?,'m_password'=?,'m_username'=? ,'m_location'=? ,'birthdate'=?,'phone_number'=?,'email'=?,'bio'=?,'agreement'=? WHERE m_username=? ; "
         self.application.db.execute(query,[name, password,username, location, birthdate, phone_number, email, bio, agreement,username])
         self.application.db.commit()
-        self.render("firstpage.html")
 
+        if call_signin==True:
+            self.redirect("/logout")
 
+        query = 'SELECT * FROM "member" WHERE "m_username"=?;'
+        cursor = self.application.db.execute(query,[current_user])
+        res = cursor.fetchone()
+        self.render("settings.html",val=res,change='true')
+
+class postHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        self.render("post.html")
+
+    @tornado.web.authenticated
+    def post(self):
+        current_user = str(self.current_user,encoding='utf-8')
+        postt = self.get_argument('posttt')
+        qu="SELECT m_name from 'member' WHERE m_username = ?"
+        namee = self.application.db.execute(qu,[current_user])
+        name = namee.fetchone()
+        print(name)
+        query = "INSERT INTO posts VALUES (?,?,?,?,?);"
+        self.application.db.execute(query,[name[0],current_user,postt,date,0])
+        self.application.db.commit()
+        self.write("successfully")
 
 
 class SignInHandler(BaseHandler):
@@ -63,15 +98,34 @@ class SignInHandler(BaseHandler):
 
         else:
             self.set_secure_cookie("username",res[1])
-            self.render("firstpage.html")
+            self.redirect('/firstpage')
 class ForgetPassHandler(BaseHandler):
     def get(self):
         self.render("forgetpass.html")
 
+
+class profile(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        current_user = str(self.current_user,encoding='utf-8')
+        query = 'SELECT * FROM "member" WHERE "m_username"=?;'
+        cursor = self.application.db.execute(query,[current_user])
+        res = cursor.fetchone()
+
+        queryy='SELECT * FROM "posts" WHERE m_username=?'
+        cursorr = self.application.db.execute(queryy,[current_user])
+        post = cursorr.fetchall()
+        length = len(post)
+        self.render("Profile.html",post=post,val=res,length=length)
+
 class firstpage(BaseHandler):
     @tornado.web.authenticated
     def get(self):
-        self.render("firstpage.html")
+        query = " select * from posts"
+        cursor = self.application.db.execute(query)
+        post = cursor.fetchall()
+        length = len(post)
+        self.render("firstpage.html",post=post,length=length)
 
 class LogOut(BaseHandler):
     @tornado.web.authenticated
@@ -99,8 +153,8 @@ class SignUpHandler(BaseHandler):
         birthdate = self.get_argument('birthdate')
         agreement = self.get_argument('agreement')
         self.application.db.execute(
-            "INSERT INTO member VALUES(?,?,?,?,?,?,?,?,?,?,?);",
-            [name,username,password,location,birthdate,phone_number,email,bio,agreement,0,0]
+            "INSERT INTO member VALUES(?,?,?,?,?,?,?,?,?);",
+            [name,username,password,location,birthdate,phone_number,email,bio,agreement]
         )
         self.application.db.commit()
         self.render("Signin.html",greeting='true',wrong='false')
@@ -123,7 +177,9 @@ def make_app():
         (r"/logout",LogOut),
         (r"/firstpage",firstpage),
         (r"/about",about),
-        (r"/settings",setting)
+        (r"/settings",setting),
+        (r"/profile",profile),
+        (r"/post",postHandler)
     ], **settings)
 
 
